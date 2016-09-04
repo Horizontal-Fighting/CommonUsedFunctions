473f103913fab77e9d3f2423b2a5c627e0f0ff0a
memoryfraction <rong.fan1031@gmail.com>
2016-09-04 00:48:20 -0500

    Async&Await

---
+++ Async&Await/Async_Await/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Async_Await
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method1();
+            Method2();
+            Console.WriteLine("Main Thread");
+            Console.ReadLine();
+        }
+
+        public static void Method1()
+        {
+            Task.Run(new Action(LongTask));
+            Console.WriteLine("Normal New Thread");
+        }
+
+        public static async void Method2()
+        {
+            await Task.Run(new Action(LongTask));
+            Console.WriteLine("New Async Thread");//wait until the long task finishes
+        }
+
+        public static void LongTask()
+        {
+            System.Threading.Thread.Sleep(5000);
+        }
+    }
+}
