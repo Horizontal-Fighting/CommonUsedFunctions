012005035e4a4b6b1940c04e510bcd0a80587ec9
memoryfraction <rong.fan1031@gmail.com>
2016-09-05 09:26:36 -0500

    DesignPatterns

---
+++ DesignPatterns/DesignPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Created by:Rong Fan
+ *email:rong.fan1031@gmail.com
+ *Desc: disign patterns
+ *Dt: 2016-9-5
+ * Version:.NET 2.0
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ DesignPatterns/DesignPattern/Singleton.cs
@@ -0,0 +1,41 @@
+﻿/* Created by:Rong Fan
+ * email:rong.fan1031@gmail.com
+ * Desc: sigleton design pattern
+ * Dt: 2016-9-5
+ * Version:.NET 2.0
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern
+{
+    public class Singleton
+    {
+
+    }
+
+
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public class SingletonProvider
+    {
+        private static Singleton instance;
+        private static readonly object syncRoot = new object();
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Singleton();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+}
