afd5b226ac19e3a08c31c8f94df615c0008d67c7
Rong Fan <rex.fan16@gmail.com>
2017-07-20 16:13:03 -0400

    change method of pass parameter

--- WebApplication_JS_RSA/WebApplicationRSA/Controllers/AccountController.cs
+++ WebApplication_JS_RSA/WebApplicationRSA/Controllers/AccountController.cs
@@ -66,7 +66,7 @@ namespace WebApplicationRSA.Controllers
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Login()
+        public async Task<ActionResult> Login(LoginModel loginModel)
         {
             var form = Request.Form;
             string plainTextPassword = form["plainTextPassword"].ToString();
